75f960884163a9bd701e29771c1a315bafbb3411
Sergey Ovcharenko <s_ovcharenko@outlook.com>
2019-01-25 14:37:04 -0500

    initial

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Plugin Name: Border Box
+ * Plugin URI: https://github.com/sergeycode/gutenblock-border-box
+ * Author: Sergey Ovcharenko
+ * Version: 1.0.0
+ * 
+ */
+
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there, hacker! Sorry, but direct access is not allowed.';
+	exit;
+}
+
+ function loadMyBlockFiles(){
+   wp_enqueue_script(
+     'border-box',
+     plugin_dir_url(__FILE__ ) . 'my-block.js',
+     array('wp-blocks', 'wp-i18n', 'wp-editor'),
+     true
+   );
+ }
+
+ add_action('enqueue_block_editro_assets', 'loadMyBlockFiles');
